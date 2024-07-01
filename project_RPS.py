c6859a058b6e9aed8db13996eeb7b5e1c8065fe6
rabongee <rmarkdehdals@gmail.com>
2024-07-01 12:36:18 +0900

    1차 완성

--- project_RPS.py
+++ project_RPS.py
@@ -8,25 +8,53 @@ def result(num1, num2):
     elif num1 == num2:
         return 0
 
+def game_continue():
+    choice = input('다시 하시겠습니까? (Y/N) ')
+    choice = choice.upper()
+    if choice != 'Y' and choice != 'N':
+        while True:
+            choice = input('Y나 N만 입력해주세요. (Y/N) ')
+            choice = choice.upper()
+            if choice == 'Y' or choice == 'N':
+                break
+    if choice == 'N':
+        return 0
+    elif choice == 'Y':
+        return 1
 
+win_count = 0
+lose_count = 0
+draw_count = 0
 
-while True: #플레이어 가위바위보 선택
-    player = input("가위 바위 보를 선택하세요 : ")
-    if player == "바위":
-        player = 0
-        break
-    elif player == "보":
-        player = 1
-        break
-    elif player == "가위":
-        player = 2
-        break
+while True:
+    while True: #플레이어 가위바위보 선택
+        player = input("가위 바위 보를 선택하세요 : ")
+        if player == "바위":
+            player = 0
+            break
+        elif player == "보":
+            player = 1
+            break
+        elif player == "가위":
+            player = 2
+            break
+        else:
+            print("가위, 바위, 보 중에서 입력해야 합니다")
+    computer = randint(0, 2) # 주먹은 0, 보는 1, 가위는 2
+    if result (player, computer) == 1:
+        win_count += 1
+        print("플레이어 WIN")
+    elif result (player, computer) == 2:
+        lose_count += 1
+        print("컴퓨터 WIN")
     else:
-        print("가위, 바위, 보 중에서 입력해야 합니다")
-computer = randint(0, 2) # 주먹은 0, 보는 1, 가위는 2
-if result (player, computer) == 1:
-    print("플레이어 WIN")
-elif result (player, computer) == 2:
-    print("컴퓨터 WIN")
-else:
-    print("비겼습니다")
+        draw_count += 1
+        print("비겼습니다")
+    player_choice = game_continue()
+    if player_choice == 0:
+        break
+    elif player_choice == 1:
+        continue
+
+print("게임을 종료합니다.")
+print(f"승리 : {win_count}, 패배 : {lose_count}, 무승부 : {draw_count}")
